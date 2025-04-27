60ee974085ec7fa72ad17a4085f3d125e35f6b67
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-04-27 17:22:29 +0530

    349. Intersection of Two Arrays | Beats 100% Time 67% Space

---
+++ array/349-intersection-of-two-arrays/349-intersection-of-two-arrays-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>occ(1001);
+        for(int i : nums1) occ[i] = 1;
+        vector<int>ans;
+        for(int i : nums2){
+            if(occ[i]){
+                occ[i] = 0;
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
