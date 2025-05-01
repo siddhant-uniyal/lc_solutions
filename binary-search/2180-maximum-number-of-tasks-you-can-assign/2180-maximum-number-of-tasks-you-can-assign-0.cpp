2d89e6131857e7168d7c92d677d3000d422c63a7
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-01 09:33:27 +0530

    2180. Maximum Number of Tasks You Can Assign | Beats 0% Time 0% Space

---
+++ binary-search/2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign-0.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        /*
+        sort tasks and workers
+        if you can give pill , give it
+
+        10 12
+
+        5 , 6 pills=1 , str = 4
+
+        i , j , x
+
+        if i give : 1 + (i + 1 , j + 1 , x - 1)
+        i dont : (i , j + 1 , x)
+
+
+        */
+        int n = tasks.size();
+        int m = workers.size();
+        vector<int>ids1(n);
+        vector<int>ids2(m);
+        iota(ids1.begin() , ids1.end() , 0);
+        iota(ids2.begin() , ids2.end() , 0);
+        sort(ids1.begin() , ids1.end() , [&](const int &a , const int &b){
+            return tasks[a] < tasks[b];
+        });
+        sort(ids2.begin() , ids2.end() , [&](const int &a , const int &b){
+            return workers[a] < workers[b];
+        });
+        int i = 0 , j = 0;
+        int ans = 0;
+        while(i < n and j < m){
+            if(workers[ids2[j]] >= tasks[ids1[i]]){
+                ++ans;
+                ++i , ++j;
+            }
+            else{
+                if(pills > 0){
+                    if(workers[ids2[j]] + strength >= tasks[ids1[i]]){
+                        ++ans;
+                        ++i , ++j;
+                        --pills;
+                    }
+                    else ++j;
+                }
+                else ++j;
+            }
+        }
+        return ans;
+    }
+};
