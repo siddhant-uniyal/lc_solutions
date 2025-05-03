2ba70effcd83def6278eaac65deb0b0368e3e295
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-03 11:05:12 +0530

    2413. Smallest Number in Infinite Set | Beats 0% Time 0% Space

---
+++ heap-priority-queue/2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set-0.cpp
@@ -0,0 +1,46 @@
+class SmallestInfiniteSet {
+public:
+    int l;
+    set<int>st;
+    SmallestInfiniteSet() {
+       l = 1; 
+    }
+
+    // [1 , inf]
+
+    // [2 , inf]
+
+    // [1 , inf]
+
+    // [3 , inf]
+
+    // [4 , inf]
+
+    // [1 , 4 , inf]
+    
+    int popSmallest() {
+        int to_return = -1;
+        if(!st.empty() and *st.begin() < l){
+            to_return = *st.begin();
+            st.erase(st.begin());
+        }
+        else{
+            to_return = l;
+            ++l;
+        }
+        return to_return;
+    }
+    
+    void addBack(int num) {
+        if(num == l - 1) --l;
+        else if(num < l) st.insert(num); 
+    }
+    // l = 5 , st = {}
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
