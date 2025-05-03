7cdd9a1b176ac7cddbab7eb69211e8cd96558f6c
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-03 12:17:49 +0530

    3498. Minimum Array Changes to Make Differences Equal | Beats 0% Time 0% Space

---
+++ hash-table/3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minChanges(vector<int>& nums, int k) {
+        // 0 1 2 3 3 6 5 4
+
+        /*
+        4 : 3
+        0 : 1 
+        */
+
+        // 2
+        // 4
+        // 1
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        int mxOcc = 0 , req = 1e5 + 1;
+        for(int i = 0 ; i < n/2 ; ++i){
+            int diff = abs(nums[i] - nums[n - i - 1]);
+            ++mp[diff];
+            if(mp[diff] >= mxOcc){
+                mxOcc = mp[diff];
+                req = min(req , diff);
+            }
+        }
+        // cout << req << endl;
+        int ans = 0;
+        for(int i = 0 ; i < n/2 ; ++i){
+            int diff = abs(nums[i] - nums[n - i - 1]);
+            if(diff == req) continue;
+            if(nums[i] + req > k and nums[i] - req < 0) ans += 2;
+            else ++ans;
+        }
+        return ans;
+    }
+};
