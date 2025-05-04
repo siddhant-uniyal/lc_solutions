9ec5a603c9efb51c90e21ed3712c43ed5da1d99d
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-04 11:09:58 +0530

    3833. Merge Operations for Minimum Travel Time | Beats 27% Time 54% Space

---
+++ dynamic-programming/3833-merge-operations-for-minimum-travel-time/3833-merge-operations-for-minimum-travel-time-0.cpp
@@ -0,0 +1,45 @@
+int dp[50][50][101][11];
+class Solution {
+public:
+    int minTravelTime(int l, int n, int k, vector<int>& position, vector<int>& time) {
+        if(n == 2) return l * time[0];
+        /*
+        at ind , i can merge with ind - 1 or not.
+        if i dont (close the subarray) , ind - 1 is finalized , so calculate cost right now and add
+
+        ind , l , prevSum , merges : at index ind , the previous subarray starts at l , the sum of 2nd previous subarray is prevSum , remaining merges = merges
+
+        if i choose to close here , ind + 1 must get currSum. that doesnt need to be maintained , prefix sum
+
+        if i choose to keep open (merge) , prev Sum DOES NOT CHANGE.
+        */
+        memset(dp , -1 , sizeof(dp));
+        vector<int>pref(n);
+        for(int i = 0 ; i < n ; ++i){
+            pref[i] = (i ? pref[i - 1] : 0) + time[i];
+        }
+
+        auto dfs = [&](int ind , int l , int prevSum , int merges , auto &&self) -> int{
+            if(ind == n){
+                int closeCost = prevSum * (position[n - 1] - position[l - 1]);
+                return merges == 0 ? closeCost : 1e9;
+            }
+            if(merges < 0) return 1e9;
+            if(dp[ind][l][prevSum][merges] != -1) return dp[ind][l][prevSum][merges];
+            /*
+            0 , 3 , 8 , 10
+            8 recieves : [0] [3) 8 10 => ) means its open
+            10 recieves : [0] [3] [8) 10 OR [0] [3 8) 10
+
+            so at 8 , closing it will use prevSum ([0] sum) , and pass on prefix sum , and merges stay same.
+            */
+            int closeCost = prevSum * (position[ind - 1] - position[l - 1]);
+            int closeGive = pref[ind - 1] - pref[l - 1];
+            int closeHere = closeCost + self(ind + 1 , ind , closeGive , merges , self);
+            //keeping open == merging
+            int keepOpen = self(ind + 1 , l , prevSum , merges - 1 , self);
+            return dp[ind][l][prevSum][merges] = min(closeHere , keepOpen);
+        };
+        return dfs(2 , 1 , time[0] , k , dfs);
+    }
+};
