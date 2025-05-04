ba5904c0737246e26128ae8ded6769dcae60ded8
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-04 09:35:03 +0530

    1227. Number of Equivalent Domino Pairs | Beats 0% Time 0% Space

---
+++ hash-table/1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        int n = dominoes.size();
+        int ans = 0;
+        for(int i = 0 ; i < n ; ++i){
+            for(int j = i + 1 ; j < n ; ++j){
+                for(int k = 0 ; k < 2 ; ++k){
+                    int ok = false;
+                    for(int l = 0 ; l < 2 ; ++l){
+                        if(dominoes[i][k] == dominoes[j][l]){
+                            ++ans;
+                            ok = true;
+                            break;
+                        }
+                    }
+                    if(ok) break;
+                }
+            }
+        }
+        return ans;
+    }
+};
