645eb71b7162b6913e124e1f222d465b10b8bdf8
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-07 11:43:54 +0530

    3627. Find Minimum Time to Reach Last Room I | Beats 76% Time 78% Space

---
+++ heap-priority-queue/3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i-0.cpp
@@ -0,0 +1,30 @@
+int d[5] = {-1 , 0 , 1 , 0 , -1};
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& grid) {
+        int n = grid.size() , m = grid[0].size();
+        vector<int>dist(n*m , 2e9);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        dist[0] = 0;
+        pq.emplace(0,0);
+        while(!pq.empty()){
+            auto [cost , src] = pq.top();
+            pq.pop();
+            if(src == n*m - 1) return cost;
+            if(dist[src] < cost) continue;
+            int x = src/m , y = src%m;
+            for(int i = 0 ; i < 4 ; ++i){
+                int X = x + d[i] , Y = y + d[i + 1];
+                if(min(X , Y) >= 0 and X < n and Y < m){
+                    int ncost = max(cost , grid[X][Y]) + 1;
+                    int nsrc = X*m + Y;
+                    if(dist[nsrc] > ncost){
+                        dist[nsrc] = ncost;
+                        pq.emplace(ncost , nsrc);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
