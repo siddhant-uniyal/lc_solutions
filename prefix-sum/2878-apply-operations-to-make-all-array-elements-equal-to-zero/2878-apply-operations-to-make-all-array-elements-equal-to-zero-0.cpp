b45e0fb93857a547d7c3904ea5af530d85b8e498
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-16 18:27:49 +0530

    2878. Apply Operations to Make All Array Elements Equal to Zero | Beats 0% Time 0% Space

---
+++ prefix-sum/2878-apply-operations-to-make-all-array-elements-equal-to-zero/2878-apply-operations-to-make-all-array-elements-equal-to-zero-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        queue<int>q;
+        int decrem = 0;
+        for(int i = 0 ; i < n ; ++i){
+            if(!q.empty() && i == q.front() + k){
+                int idx = q.front();
+                q.pop();
+                decrem = max(0 , decrem - nums[idx]);
+            }
+            if(nums[i] < decrem) return false;
+            if(nums[i] - decrem != 0){
+                if(i + k > n) return false;
+                decrem = nums[i];
+                q.push(i);
+            }
+        }
+        return true;
+    }
+};
