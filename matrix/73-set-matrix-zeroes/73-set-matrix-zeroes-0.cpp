e4a9e0275fe733f6798fced1a7dddca122aee3d6
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-21 07:43:01 +0530

    73. Set Matrix Zeroes | Beats 0% Time 0% Space

---
+++ matrix/73-set-matrix-zeroes/73-set-matrix-zeroes-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int>r(n) , c(m);
+        for(int i = 0 ; i < n ; ++i){
+            for(int j = 0 ; j < m ; ++j){
+                if(!matrix[i][j]){
+                    r[i] = 1 , c[j] = 1;
+                }
+            }
+        }
+        for(int i = 0 ; i < n ; ++i){
+            for(int j = 0 ; j < m ; ++j){
+                if(r[i] && c[j]) matrix[i][j] = 0;
+            }
+        }
+    }
+};
