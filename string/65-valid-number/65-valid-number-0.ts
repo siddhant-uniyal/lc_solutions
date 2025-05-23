a1594e629db95be3e4402a0196626f308da26eb6
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-23 09:55:20 +0530

    65. Valid Number | Beats 0% Time 0% Space

---
+++ string/65-valid-number/65-valid-number-0.ts
@@ -0,0 +1,6 @@
+function isNumber(s: string): boolean {
+    const p1 = /[-+]?[0-9]*.?[0-9]+[eE]?[0-9]+/g
+    const p2 = /[-+]?[0-9]*.?[0-9]+/g
+    const p2 = /[-+]?[0-9]+./g
+    return p1.test(s) || p2.test(s) || p3.test(s)
+};
